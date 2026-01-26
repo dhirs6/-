b78ebed735987f9c99557fdeeadf21c240202479
dhirs6 <dhirs630@ymail.ne.jp>
2026-01-27 03:37:01 +0900

    Update game.js

--- js/game.js
+++ js/game.js
@@ -225,8 +225,10 @@ window.onGyroAction = (a, p) => {
 
 window.onTouchAction = (a, p) => {
   if (!player) return;
-  if (a === 'left')  input.gyro.left  = p;
-  if (a === 'right') input.gyro.right = p;
+
+  if (a === 'left')  input.touch.left  = p;
+  if (a === 'right') input.touch.right = p;
+
   if (a === 'jump' && p && player.grounded) {
     player.vy = player.jump;
     player.grounded = false;
